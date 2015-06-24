1c7de97b6b1a47adae20e061f6e94fa5f66cf847
Joseph <madhatters@gmail.com>
2015-06-24 10:52:21 -0700

    Added the main scene, assortment of scripts. Player control now works.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player: MonoBehaviour
+{
+     public bool IsMobileControls;
+     public float Speed = 1;
+     public float JumpHeight = 1;
+     public int ConsecutiveJumpsAllowed;
+
+     private Vector3 movementDirection;
+     private Rigidbody2D playerRigidBody;
+     private int jumpCounter;
+
+     void Start()
+     {
+          playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
+          jumpCounter = 0;
+     }
+     void Update()
+     {
+          if(IsMobileControls == true)
+          {
+               MobileMovement();
+          }
+          else
+          {
+               KeyBoardMovement();
+          }
+     }
+
+     public void MobileMovement()
+     {
+          movementDirection = new Vector3(Input.acceleration.x, 0f, 0f);
+          movementDirection *= Speed;
+          foreach (Touch touch in Input.touches)
+          {
+               if (touch.phase == TouchPhase.Began)
+               {
+                    JumpPlayer();
+               }
+          }
+
+          MovePlayer();
+     }
+
+     public void KeyBoardMovement()
+     {
+          movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+          movementDirection *= Speed;
+          if(Input.GetButtonDown("Jump"))
+          {
+               JumpPlayer();
+          }
+          MovePlayer();
+     }
+
+     private void MovePlayer()
+     {
+          transform.Translate(movementDirection * Time.deltaTime);
+     }
+
+     private void JumpPlayer()
+     {
+          if(jumpCounter < ConsecutiveJumpsAllowed)
+          {
+               playerRigidBody.AddForce(new Vector2(0f, JumpHeight));
+               jumpCounter++;
+          }
+     }
+
+
+     public void OnCollisionEnter2D(Collision2D collision)
+     {
+          if (collision.gameObject.tag == "Platform")
+          {
+               ResetJumpCounter();
+          }
+     }
+
+     private void ResetJumpCounter()
+     {
+          jumpCounter = 0;
+     }
+}
